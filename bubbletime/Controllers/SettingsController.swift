54b1d57ae5c191b4e2c47a313a385c4b5d9106d7
Jonathan Moallem <jdmoallem@gmail.com>
2018-05-04 22:32:10 +1000

    Added settings screen and icon

---
+++ bubbletime/Controllers/SettingsController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsController.swift
+//  bubbletime
+//
+//  Created by Jonathan Moallem on 4/5/18.
+//  Copyright © 2018 Sudo-Code Software. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
